d2ba6fd3d0d19d02e885b5dfdf1eb6ce9130b254
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-03-06 16:10:39 -0500

    added sum for floats with float64

--- program1.go
+++ program1.go
@@ -13,7 +13,7 @@ func main() {
 	var stringOne string = "String one."
 	stringTwo := "String two."
 
-	var floatNumber float32 = 391042.49502
+	var floatNumber float32 = 3942.49502
 	floatPi := 3.14159265358979
 
 	var defaultBoolean bool = false
@@ -26,10 +26,15 @@ func main() {
 	fmt.Printf("Booleans: %t, %t\n", defaultBoolean, trueBoolean)
 
 	fmt.Printf("Sum of Numbers: %v\n", sum(defaultNumber16, number18))
+	fmt.Printf("Sum of Floats: %v\n", sum_float(floatNumber, floatPi))
 
 }
 
 func sum(num1 int, num2 int) int {
 	var result int = num1 + num2
 	return result
+}
+
+func sum_float(num1 float32, num2 float64) float64 {
+	return float64(num1) + num2
 }
